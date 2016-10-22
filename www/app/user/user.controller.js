24af4317a8840e21d70a6ec6123da5a2a02dd1dc
Pedro Villa <p.villa.lucero@gmail.com>
2016-10-22 01:03:15 -0300

    editar perfil

--- www/app/user/user.controller.js
+++ www/app/user/user.controller.js
@@ -6,9 +6,9 @@
     .controller('userController', userController)
     .controller('historialController',historialController);
 
-  userController.$inject = ['$scope','Camera','$log','$ionicPopup','profileFactory','commonService','$rootScope','storageService','utilsFactory','$state'];
+  userController.$inject = ['$scope','Camera','$log','$ionicPopup','profileFactory','commonService','$rootScope','storageService','utilsFactory','$state','$filter'];
   historialController.$inject = ['$scope','profileFactory','guidesFactory','commonService','$state']
-  function userController($scope,Camera,$log,$ionicPopup,profileFactory,commonService,$rootScope,storageService,utilsFactory,$state) {
+  function userController($scope,Camera,$log,$ionicPopup,profileFactory,commonService,$rootScope,storageService,utilsFactory,$state,$filter) {
     //forzar salida backbutton
     $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
         viewData.enableBack = true;
@@ -16,10 +16,32 @@
     var vm = this;
 
     $log.log('user');
+      vm.generos= [{
+          "id" : "MASCULINO",
+          "nombre": "Masculino"
+      },{
+          "id" : "FEMENINO",
+          "nombre": "Femenino"
+      }];
+
       var callPerfil = profileFactory.getProfile();
       callPerfil.then(
           function (data) {
               vm.perfil = data;
+              console.log(data);
+              vm.nombre = data.nombre;
+              var fecha = new Date(data.fechaNacimiento);
+              fecha.setDate(fecha.getDate()+1);
+              console.log(fecha);
+              vm.fechaNacimiento = fecha;
+              console.log(vm.fechaNacimiento);
+
+              angular.forEach(vm.generos, function(currentGenero) {
+                  if(currentGenero.id == data.genero){
+                      vm.genero = currentGenero;
+                  }
+              });
+
               vm.perfil.avatarPerfil = commonService.getFileUrl(data.avatar);
               var traerInstitucion = commonService.getResource("resource/institucion/"+vm.perfil.idInstitucion);
               traerInstitucion.then(
@@ -34,61 +56,89 @@
               traerIntereses.then(
                   function(int){
                       vm.perfil.intereses = int._embedded.enfermedades;
-                      console.log(vm.perfil);
+
+                      var traerEnfermedad = commonService.getResource("resource/enfermedad");
+                      traerEnfermedad.then(
+                          function(int){
+                              vm.enfermedades = int._embedded.enfermedades;
+                              console.log(vm.enfermedades);
+                              console.log(vm.perfil.intereses);
+                              angular.forEach(vm.enfermedades, function(currentInteres) {
+                                  if(currentInteres.id == vm.perfil.intereses[0].id){
+                                      vm.enfermedad = currentInteres;
+                                  }
+                              });
+                          },
+                          function(e){
+                              console.error(e);
+                          }
+                      );
                   },
                   function(e){
                       console.error(e);
                   }
               );
+
+              var traeInstituciones = utilsFactory.getInstituciones();
+              traeInstituciones.then(
+                  function(data){
+                      vm.instituciones = data._embedded.institucions;
+                      angular.forEach(vm.instituciones, function(current) {
+                          if(vm.perfil.idInstitucion == current.id){
+                              vm.institucion = current;
+                          }
+                      });
+
+                      console.log(vm.instituciones);
+                  },
+                  function(e){
+                      console.error(e);
+                  }
+              );
+              var traeIsapres = utilsFactory.getIsapres();
+              traeIsapres.then(
+                  function(data){
+                      vm.isapres = data._embedded.isapres;
+                      console.log(vm.isapres);
+                      angular.forEach(vm.isapres, function(current) {
+                          if(vm.perfil.isapre == current.nombre){
+                              vm.isapre = current;
+                          }
+                      });
+                  },
+                  function(e){
+                      console.error(e);
+                  }
+              );
+
+              /**/
+
+
+
           },
           function (e) {
               console.error(e);
           }
       );
-      var traeInstituciones = utilsFactory.getInstituciones();
-      traeInstituciones.then(
-          function(data){
-              vm.instituciones = data._embedded.institucions;
-              angular.forEach(vm.instituciones, function(current) {
-                if(vm.perfil.idInstitucion == current.id){
-                    vm.institucion = current;
-                }
-              });
-
-              console.log(vm.instituciones);
-          },
-          function(e){
-              console.error(e);
-          }
-      );
-      var traeIsapres = utilsFactory.getIsapres();
-      traeIsapres.then(
-          function(data){
-              vm.isapres = data._embedded.isapres;
-              /*angular.forEach(vm.isapres, function(current) {
-                  if(vm.perfil.isapre == current.id){
-                      vm.institucion = current;
-                  }
-              });*/
-          },
-          function(e){
-              console.error(e);
-          }
-      );
-
-      var traerEnfermedad = commonService.getResource("resource/enfermedad");
-      traerEnfermedad.then(
-          function(int){
-              vm.enfermedades = int._embedded.enfermedades;
-              console.log(vm.enfermedades);
-          },
-          function(e){
-              console.error(e);
-          }
-      );
 
     vm.save = function(){
-        $state.go('app.home');
+        var params= {};
+        params.nombre = vm.nombre;
+        params.isapre = vm.isapre.nombre;
+        params.fechaNacimiento = $filter('date')(vm.fechaNacimiento, "yyyy-MM-dd");
+        params.genero = vm.genero.id;
+        params.idInstitucion = vm.institucion.id;
+        params.intereses = [];
+        params.intereses.push({id: vm.enfermedad.id, nombre:vm.enfermedad.id, descripcion: vm.enfermedad.descripcion});
+        console.log(JSON.stringify(params));
+        var editarPerfil = commonService.patch("perfil",params);
+        editarPerfil.then(
+            function(response){
+                console.log(response)
+                $state.go('app.home');
+            }
+        );
+
     };
     //select cam
     // Triggered on a button click, or some other target
